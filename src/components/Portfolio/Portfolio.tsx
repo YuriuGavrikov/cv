e329cbd697a4bc003c09464ebfeb5762014ec135
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2024-01-05 14:25:01 +0300

    edit styles, add portfolio items

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,7 +1,20 @@
+import styles from "./Portfolio.module.css";
+
 export default function Portfolio() {
 	return (
-		<>
-			<h1>portfolio</h1>
-		</>
+		<div className={styles.portfolio}>
+			<div className={styles.portfolio__item}>
+				<h3 className={styles.item__title}>Landing page | HTML, CSS, JS |</h3>
+				<div className={styles.item__img}>
+					<img src="./portfolio/lending.png" alt="lending" />
+				</div>
+			</div>
+			<div className={styles.portfolio__item}>
+				<h3 className={styles.item__title}>Weather app | Vue3, SASS |</h3>
+				<div className={styles.item__img}>
+					<img src="./portfolio/weather.png" alt="weather" />
+				</div>
+			</div>
+		</div>
 	);
 }
