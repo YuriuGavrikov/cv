f959601d98b4e1e67472fa12baedc532d6fbbfa1
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2024-03-01 23:13:10 +0300

    add dist

--- src/components/generic/Success/SuccessCard.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./SuccessCard.module.css";
-
-export default function SuccessCard() {
-	return (
-		<div className={styles.successCard}>
-			<p>Ваша заяка отправлена!</p>
-		</div>
-	);
-}
